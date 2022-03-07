8582394f797ffaed0d5b169ae3f2574ea1fabecc
MarcRamos2 <91335337+MarcRamos2@users.noreply.github.com>
2022-03-07 13:14:14 +0100

    aa

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.A)) 
+        {
+
+            rb.AddForce(Vector2.left);
+        
+        }
+
+
+        if (Input.GetKey(KeyCode.D))
+        {
+
+            rb.AddForce(Vector2.right);
+
+        }
+
+    }
+}
