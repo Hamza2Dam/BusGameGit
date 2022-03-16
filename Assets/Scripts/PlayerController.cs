58092012071ef642c7ce0f65556595b2dfb7388a
MarcRamos2 <91335337+MarcRamos2@users.noreply.github.com>
2022-03-16 15:42:16 +0100

    sssds
    s

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -31,12 +31,7 @@ public class PlayerController : MonoBehaviour
 
         }
 
-        if (Input.GetKey(KeyCode.W))
-        {
-
-            rb.AddForce(Vector2.up);
-
-        }
+      
 
 
     }
