7f31addb79f54d585cad7200f6f4d3f499819cbf
MarcRamos2 <91335337+MarcRamos2@users.noreply.github.com>
2022-03-16 17:26:11 +0100

    Cono

--- Assets/Scripts/ScrollObjectes.cs
+++ Assets/Scripts/ScrollObjectes.cs
@@ -4,32 +4,38 @@ using UnityEngine;
 
 public class ScrollObjectes : MonoBehaviour
 {
-   
-{
     public float speed = 6f;
     private Vector3 StartPosition;
     private int limitycr1 = -5;
 
-    float randomNumber = Random.Range(-2, 2);
+    // each Egg would have a script like this:
 
-
-    // Start is called before the first frame update
     void Start()
     {
         StartPosition = transform.position;
 
     }
+    void Awake()
+    {
+    
+        Invoke("SpawnNext", 2f);
+    }
+    void SpawnNext()
+    {
+        float randomNumber = Random.Range(-2, 2);
+        GameObject FallingEggs = Instantiate(gameObject);
+        FallingEggs.transform.position = new Vector3(randomNumber, 4, 0);
+    }
 
-    // Update is called once per frame
+
     void Update()
     {
         transform.Translate(translation: Vector3.down * speed * Time.deltaTime);
 
-        if (transform.position.y < limitycr1)
-        {
+        //if (transform.position.y < limitycr1)
+        //{
 
-           
-            transform.position = StartPosition; 
-        }
+        //    transform.position = StartPosition;
+        //}
     }
 }
