f274fdea96e96eaa2460cf0da345d3c3ed7d9793
MarcRamos2 <91335337+MarcRamos2@users.noreply.github.com>
2022-03-16 17:48:59 +0100

    movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,10 @@ public class PlayerController : MonoBehaviour
 {
 
     Rigidbody2D rb;
+    private float limitesq = -1.5f;
+    private float limitdreta = 1.5f;
+    private float marge = 1.5f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,28 +19,26 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+    
 
         if (Input.GetKey(KeyCode.A)) 
         {
 
-            rb.AddForce(Vector2.left);
-        
+            // rb.AddForce(Vector2.left);
+           rb.transform.position = new Vector3(limitesq, -1, 0);
+
         }
 
 
         if (Input.GetKey(KeyCode.D))
         {
 
-            rb.AddForce(Vector2.right);
+            // rb.AddForce(Vector2.right);
+            rb.transform.position = new Vector3(limitesq + marge , -1, 0);
 
         }
 
-        if (Input.GetKey(KeyCode.W))
-        {
-
-            rb.AddForce(Vector2.up);
-
-        }
+      
 
 
 
