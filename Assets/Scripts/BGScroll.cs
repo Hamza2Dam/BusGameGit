f18925e7ecfeb62b14fde11e680f673a8c2e2ecd
Muhammad Hamza <mhamza.dam@institutcampalans.net>
2022-03-16 16:31:51 +0100

    kkk

---
+++ Assets/Scripts/BGScroll.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGScroll : MonoBehaviour
+
+{
+    public float speed = 6f;
+    private Vector3 StartPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartPosition = transform.position;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(translation: Vector3.down*speed*Time.deltaTime);
+    }
+}
