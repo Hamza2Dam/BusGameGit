23f36436436b79713a43f30f1b63f286e629b38b
MarcRamos2 <91335337+MarcRamos2@users.noreply.github.com>
2022-04-06 15:59:19 +0200

    push

--- Assets/ScriptsGame/PlayerController.cs
+++ Assets/ScriptsGame/PlayerController.cs
@@ -11,11 +11,19 @@ public class PlayerController : MonoBehaviour
     public Transform carril2;
     public Transform carril3;
     public GameObject redcar;
-   
+
+    private Vector2 startTouchPosition;
+    private Vector2 currentPosition;
+    private Vector2 endTouchPosition;
+    private bool stopTouch = false;
+
+    public float swipeRange = 50;
+    public float tapRange = 10;
+
     // public int ocupants;
     // public int maxocupants;
 
-    
+
     ScrollCarreteras scrollspeed;
 
 
@@ -33,27 +41,61 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-      
 
+        Swipe();
+    }
+
+    public void Swipe()
+    {
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            startTouchPosition = Input.GetTouch(0).position;
+        }
+
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
+        {
+            currentPosition = Input.GetTouch(0).position;
+            Vector2 Distance = currentPosition - startTouchPosition;
+
+            if (!stopTouch)
+            {
+
+                if (Distance.x < -swipeRange)
+                {
+                    //outputText.text = "Left";
+                    Left();
+                    stopTouch = true;
+                }
+                else if (Distance.x > swipeRange)
+                {
+                    //outputText.text = "Right";
+                    Right();
+                    stopTouch = true;
+                }
+                
+
+            }
+
+        }
     }
 
 
-   
+
 
     public void Right() // Girar a la dreta
     {
 
-         if (rb.transform.position.x == carril2.transform.position.x) // Si l'objecte está en el carril 2 (igualem les coordenades x) el pasem al carril3.x
+        if (rb.transform.position.x == carril2.transform.position.x) // Si l'objecte está en el carril 2 (igualem les coordenades x) el pasem al carril3.x
         {
 
             rb.transform.position = new Vector3(carril3.transform.position.x, 0, 0);
 
         }
-        else if ( rb.transform.position.x == carril1.transform.position.x) // Si l'objecte está en el carril 1 (igualem les coordenades x) el pasem al carril2.x
+        else if (rb.transform.position.x == carril1.transform.position.x) // Si l'objecte está en el carril 1 (igualem les coordenades x) el pasem al carril2.x
         {
 
             rb.transform.position = new Vector3(carril2.transform.position.x, 0, 0);
-            
+
 
         }
 
@@ -62,7 +104,7 @@ public class PlayerController : MonoBehaviour
 
     public void Left() // Girar a l'esquerra
     {
-        if ( rb.transform.position.x == carril2.transform.position.x)
+        if (rb.transform.position.x == carril2.transform.position.x)
         {
 
             // rb.AddForce(Vector2.left);
@@ -72,20 +114,20 @@ public class PlayerController : MonoBehaviour
 
 
         }
-        else if ( rb.transform.position.x == carril3.transform.position.x)
+        else if (rb.transform.position.x == carril3.transform.position.x)
         {
 
             rb.transform.position = new Vector3(carril2.transform.position.x, 0, 0);
 
         }
-
-
-
-
-
     }
 
-    void OnTriggerEnter2D(Collider2D other) // Colisió Trigger 
+
+
+
+        //}
+
+        void OnTriggerEnter2D(Collider2D other) // Colisió Trigger 
     {
 
         if (other.gameObject.CompareTag("car")) // Si l'objecte amb el qual colisionem te un tag == "";
