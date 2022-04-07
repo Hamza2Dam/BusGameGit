64759ebbe7a45884c0df38e570aae07034641382
MarcRamos2 <91335337+MarcRamos2@users.noreply.github.com>
2022-04-07 18:55:41 +0200

    push

---
+++ Assets/ScriptsMenu/GameOver.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class GameOver : MonoBehaviour
+{
+    PlayerController gameobj;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void Retry() 
+    {
+
+        SceneManager.LoadScene("SampleScene"); // Reiniciem la partida/l'escena
+
+        gameobj.GameOver.SetActive(false);
+
+    }
+}
