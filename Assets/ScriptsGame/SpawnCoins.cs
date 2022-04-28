1dee06729a6008e990b6709a8d20a9e9955fc8c5
MarcRamos2 <91335337+MarcRamos2@users.noreply.github.com>
2022-04-28 16:00:28 +0200

    CoinsSpawn

---
+++ Assets/ScriptsGame/SpawnCoins.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnCoins : MonoBehaviour
+{
+    public Transform carril1; // Posicio del carril1 on agafarem la x
+    public Transform carril2;
+    public Transform carril3;
+
+    public GameObject coin;
+
+
+
+
+
+    private Vector3 StartPosition; // Poscio incial
+
+
+
+    // Start is called before the first frame update
+
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        // car.transform.Translate(translation: Vector3.down * speed * 2 * Time.deltaTime); // Scroll del obejcte cap avall amb un speed
+    }
+    // Update is called once per frame
+
+
+    void Awake()
+    {
+
+        InvokeRepeating("SpawnNext", 1f, 8f); // Cada x temps (xf) cridarem a la funció SpawnNext per spawnejar un nou objecte
+
+
+    }
+    void SpawnNext()
+    {
+        float randomNumber = Random.Range(1, 4); // Número random per escollir els carrils on spawnejar l'objecte de manera random
+        GameObject generate = Instantiate(coin); // Objecte a spawnejar (Instanciar)
+        GameObject generate2 = Instantiate(coin); // Objecte a spawnejar (Instanciar)
+        GameObject generate3 = Instantiate(coin); // Objecte a spawnejar (Instanciar)
+        GameObject generate4 = Instantiate(coin); // Objecte a spawnejar (Instanciar)
+        GameObject generate5 = Instantiate(coin); // Objecte a spawnejar (Instanciar)
+    
+
+
+        if (randomNumber == 1)  // Si el numero random == 1 spawneja l'objecte al carril1.
+        {
+
+            generate.transform.position = new Vector3(carril1.transform.position.x, 40, 0); // Igualem la posicio x del objecte amb la del carril corresponent.
+            generate2.transform.position = new Vector3(carril1.transform.position.x, 41, 0); // Igualem la posicio x del objecte amb la del carril corresponent.
+            generate3.transform.position = new Vector3(carril1.transform.position.x, 42, 0); // Igualem la posicio x del objecte amb la del carril corresponent.
+            generate4.transform.position = new Vector3(carril1.transform.position.x, 43, 0); // Igualem la posicio x del objecte amb la del carril corresponent.
+            generate5.transform.position = new Vector3(carril1.transform.position.x, 44, 0); // Igualem la posicio x del objecte amb la del carril corresponent.
+
+        }
+        else if (randomNumber == 2)
+        {
+            generate.transform.position = new Vector3(carril2.transform.position.x, 40, 0);
+            generate2.transform.position = new Vector3(carril2.transform.position.x, 41, 0);
+            generate3.transform.position = new Vector3(carril2.transform.position.x, 42, 0);
+            generate4.transform.position = new Vector3(carril2.transform.position.x, 43, 0);
+            generate5.transform.position = new Vector3(carril2.transform.position.x, 44, 0);
+
+        }
+
+        else if (randomNumber == 3)
+        {
+            generate.transform.position = new Vector3(carril3.transform.position.x, 40, 0);
+            generate2.transform.position = new Vector3(carril3.transform.position.x, 41, 0);
+            generate3.transform.position = new Vector3(carril3.transform.position.x, 42, 0);
+            generate4.transform.position = new Vector3(carril3.transform.position.x, 43, 0);
+            generate5.transform.position = new Vector3(carril3.transform.position.x, 44, 0);
+
+        }
+
+    }
+
+}
+
